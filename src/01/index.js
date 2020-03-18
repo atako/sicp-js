2b4929e3a5f1351cdc1e154ee12998b3be6909f7
Dmitry <riumkin@gmail.com>
2020-03-18 15:11:06 +0300

    add 1.5 exercise

--- src/01/index.js
+++ src/01/index.js
@@ -1,6 +1,9 @@
 const square = (num) => num * num
 
 export const oneThree = (x, y, z) => {
-  return square(x) + square(y) + square(z) - 
-  square(x > y ? (y > z ? z : y) : (x > z ? z : x))
+  const min = x > y ? (y > z ? z : y) : (x > z ? z : x)
+  return square(x) + square(y) + square(z) - square(min)
 }
+
+
+export const check = (x, y) => x===0 ? 0 : y
--- test/01/index.spec.js
+++ test/01/index.spec.js
@@ -1,8 +1,13 @@
-import {oneThree} from "../../src/01/index"
+import {oneThree, check} from "../../src/01/index"
 
 describe("Chapter one", ()=> {
   test("1.3", () => {
     expect(oneThree(2,3,4)).toBe(25)
     expect(oneThree(5,3,2)).toBe(34)
+  })
+
+  test("1.5", () => {
+    const p = () => p()
+    check(0, p())
   })
 })
